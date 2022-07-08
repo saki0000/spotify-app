6c11ea7408d530eeca6e2399af1dbdec9d3577b8
saki000 <hangchengtengqi@gmail.com>
2022-07-08 17:04:18 +0900

    Change Player.tsx state

--- src/component/parts/Player.tsx
+++ src/component/parts/Player.tsx
@@ -1,10 +1,14 @@
+import { Dialog } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SpotifyPlayer from "react-spotify-web-playback";
 import { selectToken } from "../../features/counter/counterSlice";
+import { selectPlayer, setIsPlayed } from "../../features/playerSlice";
 
 const Player = ({ trackUri }: any) => {
   const userToken = useSelector(selectToken);
+  const player = useSelector(selectPlayer);
+  const dispatch = useDispatch();
   const [play, setPlay] = useState(false);
 
   useEffect(() => {
@@ -14,23 +18,37 @@ const Player = ({ trackUri }: any) => {
   if (!userToken) return null;
   return (
     <>
-      <SpotifyPlayer
-        token={userToken}
-        showSaveIcon
-        callback={(state) => !state.isPlaying && setPlay(false)}
-        play={play}
-        uris={trackUri ? trackUri : []}
-        styles={{
-          activeColor: "#25262B",
-          bgColor: "#fff",
-          color: "#25262B",
-          loaderColor: "#25262B",
-          sliderColor: "#FFD43B",
-          trackArtistColor: "#5C5F66",
-          trackNameColor: "#25262B",
-          height: "65px",
+      <Dialog
+        opened={player.isPlayed}
+        onClose={() => dispatch(setIsPlayed(false))}
+        size="xl"
+        radius="md"
+        position={{ bottom: 10, right: 250 }}
+        // withCloseButton
+        style={{
+          backgroundColor: "#fff",
+          height: 100,
+          width: 950,
         }}
-      />
+      >
+        <SpotifyPlayer
+          token={userToken}
+          showSaveIcon
+          callback={(state) => !state.isPlaying && setPlay(false)}
+          play={play}
+          uris={player.isPlayed ? player.currentTrack : []}
+          styles={{
+            activeColor: "#25262B",
+            bgColor: "#fff",
+            color: "#25262B",
+            loaderColor: "#25262B",
+            sliderColor: "#FFD43B",
+            trackArtistColor: "#5C5F66",
+            trackNameColor: "#25262B",
+            height: "65px",
+          }}
+        />
+      </Dialog>
     </>
   );
 };
