25babd94dc1864915620b0bd29bdc236946427a0
saki000 <hangchengtengqi@gmail.com>
2022-07-08 17:07:13 +0900

    Change action

--- src/features/playerSlice.ts
+++ src/features/playerSlice.ts
@@ -3,19 +3,23 @@ import { RootState } from "../app/store";
 
 const initialState = {
   currentTrack: "",
+  isPlayed: false,
 };
 
 export const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
-    setPlayer: (state, action) => {
+    setPlayers: (state, action) => {
       state.currentTrack = action.payload;
+    },
+    setIsPlayed: (state, action) => {
+      state.isPlayed = action.payload;
     },
   },
 });
 
-export const { setPlayer } = playerSlice.actions;
+export const { setPlayers, setIsPlayed } = playerSlice.actions;
 
 export const selectPlayer = (state: RootState) => state.player;
 
