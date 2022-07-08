e1430ad9d36f58c41ad483bd6df17815374162dd
saki000 <hangchengtengqi@gmail.com>
2022-07-08 17:08:00 +0900

    add player's setting

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import authReducer from "../features/counter/counterSlice";
 import userReducer from "../features/userSlice";
+import playerReducer from "../features/playerSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    player: playerReducer,
   },
 });
 
